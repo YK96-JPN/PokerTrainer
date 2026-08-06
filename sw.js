49a66d455b00910caeec99e794f4a38a574746c3
YK96-JPN <yasosuke96@gmail.com>
2026-08-06 17:15:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ポーカー道場 Service Worker
 // 更新を配る時は CACHE のバージョン番号を上げること(例: v1 → v2)
-const CACHE = "poker-dojo-pwa-v6";
+const CACHE = "poker-dojo-pwa-v16";
 const ASSETS = [
   "./",
   "./index.html",
